39fc0bb6bca9ad7917a7fbe534d09326163766aa
Hearty Oh <heartyoh@hatiolab.com>
2019-06-24 01:03:52 +0900

    Update things-scene-config-webpack-loader.js

--- web-loaders/things-scene-config-webpack-loader.js
+++ web-loaders/things-scene-config-webpack-loader.js
@@ -1,69 +1,83 @@
-const path = require('path')
-const fs = require('fs')
-const loaderUtils = require('loader-utils')
+const path = require("path");
+const fs = require("fs");
+const loaderUtils = require("loader-utils");
 
 module.exports = function(content) {
-  const sceneModuleFolders = {}
+  const sceneModuleFolders = {};
 
-  const options = loaderUtils.getOptions(this) || {}
+  const options = loaderUtils.getOptions(this) || {};
 
-  var module_path = options.module_path ? options.module_path : path.resolve(__dirname, '../node_modules')
+  var module_path = options.module_path
+    ? options.module_path
+    : path.resolve(__dirname, "../node_modules");
 
   try {
-    const thingsdir = path.resolve(module_path, '@things-scene')
-    const folders = fs.readdirSync(thingsdir)
+    const thingsdir = path.resolve(module_path, "@things-scene");
+    const folders = fs.readdirSync(thingsdir);
 
     /**
-     * package.json의 things-shell 속성이 truthy 인 경우를 필터링한다.
+     * package.json의 things-factory 속성이 truthy 인 경우를 필터링한다.
      */
     folders.forEach(folder => {
       try {
-        const pkg = require(path.resolve(thingsdir, folder, 'package.json'))
-        if (pkg['things-scene']) {
-          sceneModuleFolders[pkg.name] = path.resolve(thingsdir, folder)
+        const pkg = require(path.resolve(thingsdir, folder, "package.json"));
+        if (pkg["things-scene"]) {
+          sceneModuleFolders[pkg.name] = path.resolve(thingsdir, folder);
         }
       } catch (e) {
-        console.warn(e)
+        console.warn(e);
       }
-    })
+    });
   } catch (e) {
-    console.warn('[things-scene-config-webpack-loader]', '@things-scene module folder not found.')
+    console.warn(
+      "[things-scene-config-webpack-loader]",
+      "@things-scene module folder not found."
+    );
   }
 
   try {
     /* 현재폴더의 package.json을 보고 추가한다. */
-    const cwd = process.cwd()
-    const pkg = require(path.resolve(cwd, 'package.json'))
-    if (pkg['things-scene']) {
-      sceneModuleFolders[pkg.name] = cwd
+    const cwd = process.cwd();
+    const pkg = require(path.resolve(cwd, "package.json"));
+    if (pkg["things-scene"]) {
+      sceneModuleFolders[pkg.name] = cwd;
     }
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 
-  var sceneModules = {}
+  var sceneModules = {};
 
   for (let sceneModule in sceneModuleFolders) {
-    let folder = sceneModuleFolders[sceneModule]
+    let folder = sceneModuleFolders[sceneModule];
     try {
-      sceneModules[sceneModule] = `${folder}/things-scene.config.js`
+      sceneModules[sceneModule] = `${folder}/things-scene.config.js`;
     } catch (e) {
-      console.warn('[things-scene-config-webpack-loader]', 'things-scene.config.js file not found.')
+      console.warn(
+        "[things-scene-config-webpack-loader]",
+        "things-scene.config.js file not found."
+      );
     }
   }
 
   const script =
-    'var metas = [];\n' +
+    "var metas = [];\n" +
     Object.keys(sceneModules)
       .map((sceneModule, idx) => {
-        return `import v${idx} from "${sceneModules[sceneModule]}";\nmetas[${idx}] = v${idx};`
+        return `import v${idx} from "${
+          sceneModules[sceneModule]
+        }";\nmetas[${idx}] = v${idx};`;
       })
-      .join(';\n') +
-    ';\nexport default metas;'
+      .join(";\n") +
+    ";\nexport default metas;";
 
-  console.log('***************************** [ Things Scene Components for Modeller - BEGIN ]')
-  console.log(script)
-  console.log('***************************** [ Things Scene Components for Modeller - END   ] ')
+  console.log(
+    "***************************** [ Things Scene Components for Modeller - BEGIN ]"
+  );
+  console.log(script);
+  console.log(
+    "***************************** [ Things Scene Components for Modeller - END   ] "
+  );
 
-  return script
-}
+  return script;
+};
